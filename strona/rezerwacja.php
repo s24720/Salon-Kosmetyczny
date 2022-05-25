5a72a0daf6550e765a95f2c6d647e2a4a64b74fe
Szymo <91956608+s24720@users.noreply.github.com>
2022-05-25 13:34:32 +0200

    insert

--- strona/rezerwacja.php
+++ strona/rezerwacja.php
@@ -1,1 +1,18 @@
 <?php
+    $zabieg = $_POST['zabieg'];
+    $data = $_POST['data'];
+    $informacja = $_POST['informacja'];
+
+    $sql = "INSERT INTO wizyta (Klient_id, Salon_id, Pracownik_id, czas, Zabieg_id, informacje) VALUES
+             ('1','1',1, '$data', '$zabieg','$informacja')";
+
+    $conn = new mysqli("localhost", "szymon", "haslo", "loki");;
+    if (mysqli_query($conn, $sql)) {
+        header("Location: wziyty.php");
+    } else {
+        echo "Error: " . $sql . ":-" . mysqli_error($conn);
+    }
+    mysqli_close($conn);
+?>
+
+
