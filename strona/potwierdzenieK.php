ca3aff1857bc1621a40f7f743361971e00bcdb62
Szymo <91956608+s24720@users.noreply.github.com>
2022-05-26 12:11:47 +0200

    aktualizacja potwierdzanie

--- strona/potwierdzenieK.php
+++ strona/potwierdzenieK.php
@@ -1,1 +1,17 @@
 <?php
+$potwierdzenie = $_POST['potwierdzenieK'];
+
+
+$sql = "UPDATE wizyta SET potwierdzoneK = true WHERE  id = '$potwierdzenie';";
+
+$conn = new mysqli("localhost", "szymon", "haslo", "loki");;
+if (mysqli_query($conn, $sql)) {
+    header("Location: wziyty.php?error=Rezerwacja potwierdzona");
+} else {
+    echo "Error: " . $sql . ":-" . mysqli_error($conn);
+}
+mysqli_close($conn);
+
+
+
+
